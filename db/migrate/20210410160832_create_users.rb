85a318dc7df9ab040b7f01624212f2e08a9d24c0
Chris Egolf <cae5511@gmail.com>
2021-04-10 12:14:55 -0400

    generated models and controllers

---
+++ db/migrate/20210410160832_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.string :role
+      t.string :password
+      t.integer :group
+
+      t.timestamps
+    end
+  end
+end
