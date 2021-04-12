f7d6d1e4e72315adf002f8afa10352614c78b159
Chris Egolf <cae5511@gmail.com>
2021-04-12 14:38:36 -0400

    Revert "started ui component of login page. Added header and footer files"
    This reverts commit 1c1567065ec892da01ca454f7098dcd2c59639ab.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'login_page#login'
-  #get 'login_page/login'
+  get 'login_page/login'
   get 'admin/home'
   #root login
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
