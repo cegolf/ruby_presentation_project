2cb70cb8b387ba2c66ac942281dc8304576f5bec
Chris Egolf <cae5511@gmail.com>
2021-04-12 10:18:37 -0400

    started ui component of login page. Added header and footer files

--- Gemfile
+++ Gemfile
@@ -3,8 +3,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 #bootstrap
-gem 'bootstrap'
-
 gem "bootstrap-sass"
 
 #bcrypt for login
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'login_page/login'
+  root 'login_page#login'
+  #get 'login_page/login'
   get 'admin/home'
   #root login
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
